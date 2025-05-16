e03db907f6adbe7b7c68d182e00c01d1cff57a6c
Sarah Qasim <sarahqasim024740@gmail.com>
2025-05-16 16:58:18 +0500

    Initial commit

--- Zero-Hunger-main/config/dbConnection.js
+++ Zero-Hunger-main/config/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const db =
-      "mongodb+srv://sarahqasim024740:NXmyQ474OVnCigiL@cluster0.ldd2c.mongodb.net/";
+      "mongodb+srv://sarahqasim024740:SaRaH0123@cluster0.5i2nv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
     await mongoose.connect(db);
     console.log("MongoDB connected...");
   } catch (err) {
